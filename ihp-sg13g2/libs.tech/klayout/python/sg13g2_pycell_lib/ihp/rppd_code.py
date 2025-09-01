5c9343ea4b1670cd936a37772d1ffa6fc87d44b8
Martin Jan Köhler <info@martinjankoehler.com>
2025-09-01 20:41:23 +0200

    Guard Rings: Remove DNWELL type
    Signed-off-by: Martin Jan Köhler <info@martinjankoehler.com>

--- ihp-sg13g2/libs.tech/klayout/python/sg13g2_pycell_lib/ihp/rppd_code.py
+++ ihp-sg13g2/libs.tech/klayout/python/sg13g2_pycell_lib/ihp/rppd_code.py
@@ -104,7 +104,8 @@ class rppd(DeviceBase):
         """
         Template method for subclasses to restrict the guard ring types
         """
-        return [GuardRingType.NONE, GuardRingType.NWELL, GuardRingType.DNWELL, GuardRingType.PSUB]
+        # return [GuardRingType.NONE, GuardRingType.NWELL, GuardRingType.DNWELL, GuardRingType.PSUB]
+        return [GuardRingType.NONE, GuardRingType.NWELL, GuardRingType.PSUB]
 
     def genDeviceLayout(self):
         psdlayer = Layer('pSD')
