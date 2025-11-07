7a84f1808b3d003612b29ab6daed6abd99427be4
Martin Jan Köhler <info@martinjankoehler.com>
2025-11-07 23:31:18 +0100

    KLayout P(y)Cells:  added tech info helper dataclasses for design rules
    Signed-off-by: Martin Jan Köhler <info@martinjankoehler.com>

---
+++ ihp-sg13g2/libs.tech/klayout/python/sg13g2_pycell_lib/sg13_tech_info.py
@@ -0,0 +1,235 @@
+########################################################################
+#
+# Copyright 2024 IHP PDK Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+########################################################################
+
+from __future__ import annotations
+from dataclasses import dataclass
+from functools import cached_property
+import json
+import os
+from typing import *
+
+
+NX = int
+NY = int
+SpaceLambda = Callable[[NX, NY], Tuple[float, float]]
+
+
+@dataclass
+class LayerInfo:
+    gds_layer: int
+    gds_datatype: int
+    name: str
+
+
+@dataclass
+class ViaInfo:
+    name: str
+    description: str
+    bottom: LayerInfo 
+    cut: LayerInfo
+    top: LayerInfo
+    bottom_grid: float
+    top_grid: float
+    wbmin: float
+    hbmin: float
+    wtmin: float
+    htmin: float
+    enc_bottom: float
+    enc_endcap_bottom: float
+    enc_top: float
+    enc_endcap_top: float
+    width: float
+    space_lambda: SpaceLambda
+    
+    def enc_bottom_for_nx_ny(self, nx: int, ny: int) -> (float, float):
+        if nx < ny:
+            return (self.enc_bottom, self.enc_endcap_bottom)
+        else:
+            return (self.enc_endcap_bottom, self.enc_bottom)
+
+    def enc_top_for_nx_ny(self, nx: int, ny: int) -> (float, float):
+        if nx < ny:
+            return (self.enc_top, self.enc_endcap_top)
+        else:
+            return (self.enc_endcap_top, self.enc_top)
+
+
+@dataclass
+class TechInfo:
+    layers: List[LayerInfo]
+    vias: List[ViaInfo]
+
+    @classmethod
+    def instance(cls) -> TechInfo:
+        if not hasattr(cls, '_instance'):
+            tech_file_path = os.path.join(os.path.dirname(__file__), 'sg13g2_tech.json')
+            js_data: Dict
+            with open(tech_file_path, "r") as tech_file:
+                js_data = json.load(tech_file)
+            tp = js_data['techParams']
+        
+            layers = [
+                LayerInfo(1, 0, 'Activ'),
+                LayerInfo(5, 0, 'GatPoly'),
+                LayerInfo(6, 0, 'Cont'),
+                LayerInfo(8, 0, 'Metal1'),
+                LayerInfo(19, 0, 'Via1'),
+                LayerInfo(10, 0, 'Metal2'),
+                LayerInfo(29, 0, 'Via2'),
+                LayerInfo(30, 0, 'Metal3'),
+                LayerInfo(49, 0, 'Via3'),
+                LayerInfo(50, 0, 'Metal4'),
+                LayerInfo(66, 0, 'Via4'),
+                LayerInfo(67, 0, 'Metal5'),
+                LayerInfo(125, 0, 'TopVia1'),
+                LayerInfo(126, 0, 'TopMetal1'),
+                LayerInfo(133, 0, 'TopVia2'),
+                LayerInfo(134, 0, 'TopMetal2'),
+            ]
+            ld: Dict[str, LayerInfo] = {l.name: l for l in layers}
+            
+            grid = 0.005
+            
+            vias = [
+                ViaInfo(name='SG13G2_CONT_GATPOLY_M1', description='Cont (GatPoly→Metal1)', 
+                        bottom=ld['GatPoly'], cut=ld['Cont'], top=ld['Metal1'], bottom_grid=grid, top_grid=grid, 
+                        wbmin=0.2, hbmin=0.2, wtmin=0.2, htmin=0.2,
+                        enc_bottom=tp['Cnt_d'], enc_endcap_bottom=tp['Cnt_d'],
+                        enc_top=tp['M1_c'], enc_endcap_top=tp['M1_c1'],
+                        width=tp['Cnt_a'],
+                        space_lambda=lambda nx, ny: (tp['Cnt_b1'], tp['Cnt_b']) \
+                                                    if nx > tp['Cnt_b1_nr'] and ny > tp['Cnt_b1_nr'] \
+                                                    else (tp['Cnt_b'], tp['Cnt_b'])
+                        ),
+                ViaInfo(name='SG13G2_CONT_ACTIV_M1', description='Cont (Activ→Metal1)', 
+                        bottom=ld['Activ'], cut=ld['Cont'], top=ld['Metal1'], bottom_grid=grid, top_grid=grid, 
+                        wbmin=0.2, hbmin=0.2, wtmin=0.2, htmin=0.2,
+                        enc_bottom=tp['Cnt_d'], enc_endcap_bottom=tp['Cnt_d'],
+                        enc_top=tp['M1_c'], enc_endcap_top=tp['M1_c1'],
+                        width=tp['Cnt_a'], 
+                        space_lambda=lambda nx, ny: (tp['Cnt_b1'], tp['Cnt_b']) \
+                                                    if nx > tp['Cnt_b1_nr'] and ny > tp['Cnt_b1_nr'] \
+                                                    else (tp['Cnt_b'], tp['Cnt_b'])
+                        ),
+                ViaInfo(name='SG13G2_VIA_M1_M2', description='Via1 (Metal1→Metal2)', 
+                        bottom=ld['Metal1'], cut=ld['Via1'], top=ld['Metal2'], bottom_grid=grid, top_grid=grid, 
+                        wbmin=0.2, hbmin=0.2, wtmin=0.2, htmin=0.2,
+                        enc_bottom=tp['V1_c'], enc_endcap_bottom=tp['V1_c1'],
+                        enc_top=tp['Mn_c'], enc_endcap_top=tp['Mn_c1'],
+                        width=tp['V1_a'], 
+                        space_lambda=lambda nx, ny: (tp['V1_b1'], tp['V1_b']) \
+                                                    if nx > tp['V1_b1_nr'] and ny > tp['V1_b1_nr'] \
+                                                    else (tp['V1_b'], tp['V1_b'])
+                        ),
+                ViaInfo(name='SG13G2_VIA_M2_M3', description='Via2 (Metal2→Metal3)', 
+                        bottom=ld['Metal2'], cut=ld['Via2'], top=ld['Metal3'], bottom_grid=grid, top_grid=grid, 
+                        wbmin=0.2, hbmin=0.2, wtmin=0.2, htmin=0.2,
+                        enc_bottom=tp['Vn_c'], enc_endcap_bottom=tp['Vn_c1'],
+                        enc_top=tp['Mn_c'], enc_endcap_top=tp['Mn_c1'],
+                        width=tp['Vn_a'], 
+                        space_lambda=lambda nx, ny: (tp['Vn_b1'], tp['Vn_b']) \
+                                                    if nx > tp['Vn_b1_nr'] and ny > tp['Vn_b1_nr'] \
+                                                    else (tp['Vn_b'], tp['Vn_b'])
+                        ),
+                ViaInfo(name='SG13G2_VIA_M3_M4', description='Via3 (Metal3→Metal4)', 
+                        bottom=ld['Metal3'], cut=ld['Via3'], top=ld['Metal4'], bottom_grid=grid, top_grid=grid, 
+                        wbmin=0.2, hbmin=0.2, wtmin=0.2, htmin=0.2,
+                        enc_bottom=tp['Vn_c'], enc_endcap_bottom=tp['Vn_c1'],
+                        enc_top=tp['Mn_c'], enc_endcap_top=tp['Mn_c1'],
+                        width=tp['Vn_a'], 
+                        space_lambda=lambda nx, ny: (tp['Vn_b1'], tp['Vn_b']) \
+                                                    if nx > tp['Vn_b1_nr'] and ny > tp['Vn_b1_nr'] \
+                                                    else (tp['Vn_b'], tp['Vn_b'])
+                        ),
+                ViaInfo(name='SG13G2_VIA_M4_M5', description='Via4 (Metal4→Metal5)', 
+                        bottom=ld['Metal4'], cut=ld['Via4'], top=ld['Metal5'], bottom_grid=grid, top_grid=grid, 
+                        wbmin=0.2, hbmin=0.2, wtmin=0.2, htmin=0.2,
+                        enc_bottom=tp['Vn_c'], enc_endcap_bottom=tp['Vn_c1'],
+                        enc_top=tp['Mn_c'], enc_endcap_top=tp['Mn_c1'],
+                        width=tp['Vn_a'], 
+                        space_lambda=lambda nx, ny: (tp['Vn_b1'], tp['Vn_b']) \
+                                                    if nx > tp['Vn_b1_nr'] and ny > tp['Vn_b1_nr'] \
+                                                    else (tp['Vn_b'], tp['Vn_b'])
+                    ),
+                ViaInfo(name='SG13G2_VIA_M5_TM1', description='TopVia1 (Metal5→TopMetal1)', 
+                        bottom=ld['Metal5'], cut=ld['TopVia1'], top=ld['TopMetal1'], bottom_grid=grid, top_grid=grid, 
+                        wbmin=0.2, hbmin=0.2, wtmin=tp['TM1_a'], htmin=tp['TM1_a'],
+                        enc_bottom=tp['TV1_c'], enc_endcap_bottom=tp['TV1_c'],
+                        enc_top=tp['TV1_d'], enc_endcap_top=tp['TV1_d'],
+                        width=tp['TV1_a'], 
+                        space_lambda=lambda nx, ny: (tp['TV1_b'], tp['TV1_b'])
+                        ),
+                ViaInfo(name='SG13G2_VIA_TM1_TM2', description='TopVia2 (TopMetal1→TopMetal2)', 
+                        bottom=ld['TopMetal1'], cut=ld['TopVia2'], top=ld['TopMetal2'], bottom_grid=grid, top_grid=grid, 
+                        wbmin=tp['TM1_a'], hbmin=tp['TM1_a'], wtmin=tp['TM2_a'], htmin=tp['TM2_a'],
+                        enc_bottom=tp['TV2_c'], enc_endcap_bottom=tp['TV2_c'],
+                        enc_top=tp['TV2_d'], enc_endcap_top=tp['TV2_d'],
+                        width=tp['TV2_a'], 
+                        space_lambda=lambda nx, ny: (tp['TV2_b'], tp['TV2_b'])
+                        ),
+            ]
+            
+            cls._instance = TechInfo(layers=layers, vias=vias)
+        return cls._instance
+    
+    def layer_by_name(self, name: str) -> LayerInfo:
+        return [l for l in self.layers if l.name == name][0]
+
+    def via_by_name(self, name: str) -> ViaInfo:
+        return [v for v in self.vias if v.name == name][0]
+
+    @cached_property
+    def via_layers(self) -> Set[LayerInfo]:
+        return {v.cut for v in self.vias}
+
+    @cached_property
+    def non_via_layers(self) -> List[LayerInfo]:
+        return [l for l in self.layers if not l in self.via_layers]
+    
+    @cached_property
+    def layer_choices(self) -> List[Tuple[str, LayerInfo]]:
+        return [(l.name, l) for l in self.non_via_layers]
+    
+    @cached_property
+    def via_choices(self) -> List[Tuple[str, str]]:
+        return [(v.description, v.name) for v in self.vias]
+    
+    def num_stack_steps(self, bot: LayerInfo, top: LayerInfo) -> int:
+        bi = self.layers.index(bot)
+        ti = self.layers.index(top)
+        return ti - bi
+
+    def get_vias(self, bottom: LayerInfo, top: LayerInfo) -> List[Via]:
+        bottom_via_found = False
+        already_added_cut_layers: Set[LayerInfo] = set()
+        vias = []
+        for via in self.vias:
+            if not bottom_via_found:
+                if via.bottom == bottom:
+                    vias.append(via)
+                    already_added_cut_layers.add(via.cut)
+                    bottom_via_found = True
+                    
+            if bottom_via_found:
+                if via.cut not in already_added_cut_layers:
+                    vias.append(via)
+                
+                if via.top == top:
+                    break
+
+        return vias
